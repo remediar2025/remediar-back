915db3317d0f6d54248ca81fd7a67949579deebe
José Guilherme <105402150+guilhermeverissimo14@users.noreply.github.com>
2026-03-17 22:20:10 -0300

    Fix: adiciona @ConditionalOnProperty no RedisConfig

--- back-remediar/src/main/java/com/remediar/back_remediar/infra/config/RedisConfig.java
+++ back-remediar/src/main/java/com/remediar/back_remediar/infra/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.remediar.back_remediar.infra.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -7,6 +8,7 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
+@ConditionalOnProperty(name = "spring.data.redis.host")
 public class RedisConfig {
 
     @Bean
